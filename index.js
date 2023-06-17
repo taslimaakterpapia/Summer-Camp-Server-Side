18212207ca87d9386544ceec7be72addf744c35e
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-06-17 22:21:49 +0600

    index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.PAYMENT_KEY);
 const port = process.env.PORT || 5000;
 
-// --------------------------***
+
 app.use(cors());
 app.use(express.json());
 const corsOptions = {
